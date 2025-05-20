6eb0555df4b60b176336574a12d730ea29732f1f
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-20 08:12:57 -0400

    Update DailyGame model to use JsonPropertyName attributes

--- Models/DailyGame.cs
+++ Models/DailyGame.cs
@@ -4,12 +4,16 @@ namespace ShredleApi.Models
 {
     public class DailyGame
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
         
+        [JsonPropertyName("date")]
         public DateTime Date { get; set; }
         
+        [JsonPropertyName("solo_id")]
         public int? SoloId { get; set; }
         
+        // This property is not stored in the database, but used for navigation
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Solo? Solo { get; set; }
     }
