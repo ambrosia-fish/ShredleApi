39d14e01d7f8670b89ef028c19ad4e6d4808cec9
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-20 08:52:33 -0400

    Update Solo model to use PascalCase for JsonPropertyName attributes

--- Models/Solo.cs
+++ Models/Solo.cs
@@ -5,28 +5,28 @@ namespace ShredleApi.Models
     public class Solo
     {
         // Using standard properties without Entity Framework or Postgrest dependencies
-        [JsonPropertyName("id")]
+        [JsonPropertyName("Id")]
         public int Id { get; set; }
         
-        [JsonPropertyName("title")]
+        [JsonPropertyName("Title")]
         public string Title { get; set; } = string.Empty;
         
-        [JsonPropertyName("artist")]
+        [JsonPropertyName("Artist")]
         public string Artist { get; set; } = string.Empty;
         
-        [JsonPropertyName("spotify_id")]
+        [JsonPropertyName("SpotifyId")]
         public string SpotifyId { get; set; } = string.Empty;
         
-        [JsonPropertyName("solo_start_time_ms")]
+        [JsonPropertyName("SoloStartTimeMs")]
         public int SoloStartTimeMs { get; set; }
         
-        [JsonPropertyName("solo_end_time_ms")]
+        [JsonPropertyName("SoloEndTimeMs")]
         public int SoloEndTimeMs { get; set; }
         
-        [JsonPropertyName("guitarist")]
+        [JsonPropertyName("Guitarist")]
         public string Guitarist { get; set; } = string.Empty;
         
-        [JsonPropertyName("ai_hint")]
+        [JsonPropertyName("AiHint")]
         public string AiHint { get; set; } = string.Empty;
     }
 }
