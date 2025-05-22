6ffab2a94c5e3b504a7be2e0635ed895576144ff
ambrosia-fish <josefameur@gmail.com>
2025-05-21 21:11:28 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -61,8 +61,11 @@ builder.Services.AddScoped<GuessValidationService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 else
 {
     app.UseHsts();
