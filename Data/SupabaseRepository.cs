638b40156efd9933388584182ba19cfec47b7c86
Josef Ameur <148804322+ambrosia-fish@users.noreply.github.com>
2025-05-22 15:01:43 -0400

    Add GetTestGameByDateAsync method for games_test table

--- Data/SupabaseRepository.cs
+++ Data/SupabaseRepository.cs
@@ -23,6 +23,16 @@ namespace ShredleApi.Data
             return response;
         }
 
+        public async Task<Game?> GetTestGameByDateAsync(DateTime date)
+        {
+            var response = await _supabaseClient
+                .From<Game>("games_test")
+                .Where(g => g.Date == date)
+                .Single();
+
+            return response;
+        }
+
         public async Task<Solo?> GetSoloByIdAsync(int id)
         {
             var response = await _supabaseClient
